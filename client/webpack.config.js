8bb24c3ff2f0233d37d416702d90ae4d74bd0e9f
Hyunmin <chlgusals3@gmail.com>
2022-07-05 16:29:55 +0900

    Setting : Initial setting fin?

---
+++ client/webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/App.js",
+  mode: "development",
+  output: {
+    filename: "index_output.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+
+  devServer: {
+    host: "localhost",
+    port: 7000,
+    open: true,
+    historyApiFallback: true,
+  },
+};
---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.status(200).send("Hello!");
+});
+
+server = app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
