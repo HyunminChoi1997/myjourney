231b7cc0b2fa844afa7277723ae0c06f8f14fca8
Hyunmin <chlgusals3@gmail.com>
2022-07-08 15:55:30 +0900

    fix : webpack config, added : buildspec for CI/CD

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,36 +1,38 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: "./src/App.js",
-  mode: "development",
-  output: {
-    filename: "index_output.js",
-  },
+module.exports = (env) => {
+  return {
+    entry: "./src/App.js",
+    mode: env.mode,
+    output: {
+      filename: "index_output.js",
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.(png|jpg)$/,
-        use: "file-loader",
-      },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.(png|jpg)$/,
+          use: "file-loader",
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+      }),
     ],
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./index.html",
-    }),
-  ],
-
-  devServer: {
-    host: "localhost",
-    port: 7000,
-    open: true,
-    historyApiFallback: true,
-  },
+    devServer: {
+      host: "localhost",
+      port: 7000,
+      open: true,
+      historyApiFallback: true,
+    },
+  };
 };
